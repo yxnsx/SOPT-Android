b7c87858789b087e1507d75be8f920372672323b
yunso <cw2cu96@gmail.com>
2020-10-12 17:41:39 +0900

    Add: SignUpActivity.kt 파일에 OnClickListener, Intent 추가
    * 변경 내용
    - onClickListener 추가
    - checkValidation() 추가 (editText 폼 입력상태 체크)
    - Intent 추가

--- SOPT_1010/app/src/main/java/com/yxnsx/week01/SignUpActivity.kt
+++ SOPT_1010/app/src/main/java/com/yxnsx/week01/SignUpActivity.kt
@@ -1,11 +1,45 @@
 package com.yxnsx.week01
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        // 버튼에 클릭리스너 설정
+        button_signUp.setOnClickListener(onClickListener)
+    }
+
+    private val onClickListener = View.OnClickListener() {
+        when (it) {
+            button_signUp -> {
+                // 폼 입력상태 체크
+                checkValidation()
+            }
+        }
+    }
+
+    fun checkValidation() {
+        // 모든 폼이 입력되지 않았을 경우,
+        if (editText_name.text.isEmpty() || editText_id.text.isEmpty() || editText_password.text.isEmpty()) {
+            // 토스트 메시지 츨력
+            Toast.makeText(this, "모든 폼을 입력해주세요.", Toast.LENGTH_SHORT).show()
+
+        } else { // 모든 폼이 입력되었을 경우,
+            // LogInActivity로 되돌아가는 인텐트 설정
+            val intent = Intent(this, LogInActivity::class.java)
+            intent.putExtra("userId", editText_id.text.toString())
+            intent.putExtra("userPassword", editText_password.text.toString())
+            setResult(Activity.RESULT_OK, intent)
+
+            finish()
+        }
     }
 }
