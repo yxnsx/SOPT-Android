cb0341fe502c1a0d42c95edec9186eaf8a7581b0
yunso <cw2cu96@gmail.com>
2020-10-12 19:34:46 +0900

    Fix: MySharedPreferences.kt 파일의 getString 메서드 매개변수 수정
    * 변경 내용
    - 매개변수 @Nullable defValue: String 추가

--- SOPT_1010/app/src/main/java/com/yxnsx/week01/MySharedPreferences.kt
+++ SOPT_1010/app/src/main/java/com/yxnsx/week01/MySharedPreferences.kt
@@ -2,6 +2,7 @@ package com.yxnsx.week01
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.annotation.Nullable
 
 class MySharedPreferences(context: Context) {
 
@@ -9,8 +10,8 @@ class MySharedPreferences(context: Context) {
     private val MODE = Context.MODE_PRIVATE
     private val sharedPreferences: SharedPreferences = context.getSharedPreferences(NAME, MODE)
 
-    fun getString(key: String): String {
-        return sharedPreferences.getString(key, null).toString()
+    fun getString(key: String, @Nullable defValue: String): String {
+        return sharedPreferences.getString(key, defValue).toString()
     }
 
     fun setString(key: String, value: String) {
