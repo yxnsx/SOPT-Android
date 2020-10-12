44c558d15e98b205cd851e3b408321e0537ea566
yunso <cw2cu96@gmail.com>
2020-10-12 19:05:59 +0900

    Modify: MySharedPreferences.kt 파일의 getString 메서드 매개변수 수정
    * 변경 내용
    - 매개변수 value: String 삭제

--- SOPT_1010/app/src/main/java/com/yxnsx/week01/MySharedPreferences.kt
+++ SOPT_1010/app/src/main/java/com/yxnsx/week01/MySharedPreferences.kt
@@ -9,8 +9,8 @@ class MySharedPreferences(context: Context) {
     private val MODE = Context.MODE_PRIVATE
     private val sharedPreferences: SharedPreferences = context.getSharedPreferences(NAME, MODE)
 
-    fun getString(key: String, value: String): String {
-        return sharedPreferences.getString(key, value).toString()
+    fun getString(key: String): String {
+        return sharedPreferences.getString(key, null).toString()
     }
 
     fun setString(key: String, value: String) {
