c99858fa6bc156629e1e185cbbc7bd44bc5bb643
yunso <cw2cu96@gmail.com>
2020-10-12 18:45:26 +0900

    Add: MySharedPreferences.kt 파일 추가
    * 변경 내용
    - getString 메서드 구현
    - setString 메서드 구현

---
+++ SOPT_1010/app/src/main/java/com/yxnsx/week01/MySharedPreferences.kt
@@ -0,0 +1,20 @@
+package com.yxnsx.week01
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class MySharedPreferences(context: Context) {
+
+    private val NAME = "SharedPreferences"
+    private val MODE = Context.MODE_PRIVATE
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences(NAME, MODE)
+
+    fun getString(key: String, value: String): String {
+        return sharedPreferences.getString(key, value).toString()
+    }
+
+    fun setString(key: String, value: String) {
+        sharedPreferences.edit().putString(key, value).apply()
+    }
+
+}
