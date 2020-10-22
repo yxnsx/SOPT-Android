fe83c6fdd4c2f6c93f959a3d690290a05968479b
yxnsx <cw2cu96@gmail.com>
2020-10-22 18:32:02 +0900

    Create: ProfileDetaliActivity.kt 파일 생성
    # [타입] 제목
    # 제목의 길이는 최대 50글자 (영문 기준)
    # 제목 작성 후 한줄 공백 필수
    # 제목 타입 목록
    
    #	기능 추가	:	FEAT_ADD
    #	기능 수정	:	FEAT_FIX
    #	기능 보완	:	FEAT_UPDATE
    #	기능 삭제	:	FEAT_REMOVE
    #	문서 추가	:	DOCS_ADD
    #	문서 수정	:	DOCS_FIX
    #	문서 보완	:	DOCS_UPDATE
    #	문서 삭제	:	DOCS_REMOVE
    #	버그 수정	:	BUG_FIX
    #	파일 이동	:	FILE_MOVE
    #	파일명 변경	:	FILE_RENAME
    #	변수명 변경	:	VAR_RENAME
    #	테스트	:	TEST
    #	리팩토링	:	REFACTOR
    
    # 본문
    # 본문은 한 줄에 최대 70글자 (영문 기준)
    # 변경 사항, 변경 이유 포함

---
+++ SOPT_1017/app/src/main/java/com/yxnsx/sopt_1017/ProfileDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.yxnsx.sopt_1017
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_detail_acticity)
+    }
+}
