172cfe04b902d6d8db233175dc6f4095e1f5bd90
yxnsx <cw2cu96@gmail.com>
2020-10-22 18:23:46 +0900

    Modify: ProfileData.kt 파일에 @Parcelize annotation 추가 및 생성자에 image 파라미터 추가
    * 상세 내용
    - @Parcelize annotation 추가
    - 리턴타입 Parcelable로 지정
    - 생성자에 var image: Int 추가

--- SOPT_1017/app/src/main/java/com/yxnsx/sopt_1017/ProfileData.kt
+++ SOPT_1017/app/src/main/java/com/yxnsx/sopt_1017/ProfileData.kt
@@ -1,7 +1,11 @@
 package com.yxnsx.sopt_1017
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class ProfileData (
+@Parcelize
+data class ProfileData(
+    var image: Int,
     var title: String,
     var subTitle: String
-)
+): Parcelable
