0d36e608661eb1eb826ec6879b22c2bf6041276d
yxnsx <cw2cu96@gmail.com>
2020-10-23 23:11:40 +0900

    Modify: ProfileListViewModel.kt 파일의 dummyData에 date, description 값 추가
    # [타입] 제목
    # 제목의 길이는 최대 50글자 (영문 기준)
    # 제목 작성 후 한줄 공백 필수
    # 제목 타입 목록
    
    #	기능 추가	:	FEAT_ADD
    #	기능 수정	:	FEAT_FIX
    #	기능 보완	:	FEAT_UPDATE
    #	기능 삭제	:	FEAT_REMOVE
    #	문서 추가	:	DOCS_ADD
    #	문서 수정	:	DOCS_FIX
    #	문서 보완	:	DOCS_UPDATE
    #	문서 삭제	:	DOCS_REMOVE
    #	버그 수정	:	BUG_FIX
    #	파일 이동	:	FILE_MOVE
    #	파일명 변경	:	FILE_RENAME
    #	변수명 변경	:	VAR_RENAME
    #	테스트	:	TEST
    #	리팩토링	:	REFACTOR
    
    # 본문
    # 본문은 한 줄에 최대 70글자 (영문 기준)
    # 변경 사항, 변경 이유 포함

--- SOPT_1017/app/src/main/java/com/yxnsx/sopt_1017/ProfileListViewModel.kt
+++ SOPT_1017/app/src/main/java/com/yxnsx/sopt_1017/ProfileListViewModel.kt
@@ -14,16 +14,16 @@ class ProfileListViewModel : ViewModel() {
 
     private fun setDummyData(): ArrayList<ProfileData> {
         return arrayListOf(
-            ProfileData(R.drawable.emoji_01, "이름", "최윤소"),
-            ProfileData(R.drawable.emoji_02, "나이", "25"),
-            ProfileData(R.drawable.emoji_03, "파트", "안드로이드"),
-            ProfileData(R.drawable.emoji_04, "github", "https://github.com/yxnsx"),
-            ProfileData(R.drawable.emoji_05, "SOPT", "www.sopt.org"),
-            ProfileData(R.drawable.emoji_06, "이름", "최윤소"),
-            ProfileData(R.drawable.emoji_07, "나이", "25"),
-            ProfileData(R.drawable.emoji_08, "파트", "안드로이드"),
-            ProfileData(R.drawable.emoji_09, "github", "https://github.com/yxnsx"),
-            ProfileData(R.drawable.emoji_10, "SOPT", "www.sopt.org")
+            ProfileData(R.drawable.emoji_01, "이름", "최윤소", "2020-10-23", "안녕하세요! 최윤소입니다.."),
+            ProfileData(R.drawable.emoji_02, "나이", "25", "2020-10-23", "나이는 25살입니다.."),
+            ProfileData(R.drawable.emoji_03, "파트", "안드로이드", "2020-10-23", "우당탕탕 안드로이드 파트입니다.."),
+            ProfileData(R.drawable.emoji_04, "github", "https://github.com/yxnsx", "2020-10-23", "깃허브 링크입니다.."),
+            ProfileData(R.drawable.emoji_05, "SOPT", "www.sopt.org", "2020-10-23", "27기 ON SOPT 열심히 하겠습니다.."),
+            ProfileData(R.drawable.emoji_06, "이름", "최윤소", "2020-10-23", "안녕하세요! 최윤소입니다.."),
+            ProfileData(R.drawable.emoji_07, "나이", "25", "2020-10-23", "나이는 25살입니다.."),
+            ProfileData(R.drawable.emoji_08, "파트", "안드로이드", "2020-10-23", "우당탕탕 안드로이드 파트입니다.."),
+            ProfileData(R.drawable.emoji_09, "github", "https://github.com/yxnsx", "2020-10-23", "깃허브 링크입니다.."),
+            ProfileData(R.drawable.emoji_10, "SOPT", "www.sopt.org", "2020-10-23", "27기 ON SOPT 열심히 하겠습니다..")
         )
     }
 }
