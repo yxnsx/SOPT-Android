3512e619edd09ebaab2086fe681b7a4dea20646a
yxnsx <cw2cu96@gmail.com>
2020-11-05 18:30:37 +0900

    Create: bottomNavigation에 들어갈 프레그먼트 생성
    * 상세 내용
    - ProfileFragment.kt 생성
    - RecyclerViewFragment 생성
    - EmptyFragment 생성
    
    # [타입] 제목
    # 제목의 길이는 최대 50글자 (영문 기준)
    # 제목 작성 후 한줄 공백 필수
    # 제목 타입 목록
    
    #	기능 추가	:	FEAT_ADD
    #	기능 수정	:	FEAT_FIX
    #	기능 보완	:	FEAT_UPDATE
    #	기능 삭제	:	FEAT_REMOVE
    #	문서 추가	:	DOCS_ADD
    #	문서 수정	:	DOCS_FIX
    #	문서 보완	:	DOCS_UPDATE
    #	문서 삭제	:	DOCS_REMOVE
    #	버그 수정	:	BUG_FIX
    #	파일 이동	:	FILE_MOVE
    #	파일명 변경	:	FILE_RENAME
    #	변수명 변경	:	VAR_RENAME
    #	테스트	:	TEST
    #	리팩토링	:	REFACTOR
    
    # 본문
    # 본문은 한 줄에 최대 70글자 (영문 기준)
    # 변경 사항, 변경 이유 포함

---
+++ app/src/main/java/com/yxnsx/sopt/week03/ProfileFragment.kt
@@ -0,0 +1,27 @@
+package com.yxnsx.sopt.week03
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yxnsx.sopt.R
+
+
+class ProfileFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        return view
+    }
+
+    // ui 작업 수행
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/yxnsx/sopt/week03/RecyclerViewFragment.kt
+++ app/src/main/java/com/yxnsx/sopt/week03/RecyclerViewFragment.kt
@@ -1,37 +1,49 @@
-package com.yxnsx.sopt.week02
+package com.yxnsx.sopt.week03
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.yxnsx.sopt.*
+import com.yxnsx.sopt.R
 import com.yxnsx.sopt.databinding.ActivityHomeBinding
+import com.yxnsx.sopt.databinding.FragmentRecyclerViewBinding
+import com.yxnsx.sopt.week02.*
 
 
-class HomeActivity : AppCompatActivity(),
+class RecyclerViewFragment : Fragment(),
     ProfileItemClickListener,
     ProfileItemDragListener,
     ProfileItemActionListener {
 
     private val dataList = mutableListOf<ProfileData>()
-    private lateinit var viewBinding: ActivityHomeBinding
+    private lateinit var viewBinding: FragmentRecyclerViewBinding
     private val profileListViewModel: ProfileListViewModel by viewModels()
 
     private lateinit var itemTouchHelper: ItemTouchHelper
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = FragmentRecyclerViewBinding.inflate(layoutInflater)
+        return viewBinding.root
+    }
 
-        // viewBinding 설정
-        viewBinding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
+    // ui 작업 수행
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
 
         // clickListener 설정
         viewBinding.FABGridLayout.setOnClickListener(onClickListener)
@@ -40,8 +52,8 @@ class HomeActivity : AppCompatActivity(),
         viewBinding.recyclerView.apply {
             adapter = ProfileAdapter(
                 dataList,
-                this@HomeActivity,
-                this@HomeActivity
+                this@RecyclerViewFragment,
+                this@RecyclerViewFragment
             )
             layoutManager = LinearLayoutManager(context)
         }
@@ -54,7 +66,7 @@ class HomeActivity : AppCompatActivity(),
         itemTouchHelper.attachToRecyclerView(viewBinding.recyclerView)
 
         // 뷰모델의 Observer를 통해 리사이클러뷰의 ProfileAdapter에 변경값 갱신
-        profileListViewModel.profileLiveData.observe(this, Observer {
+        profileListViewModel.profileLiveData.observe(viewLifecycleOwner, Observer {
             (viewBinding.recyclerView.adapter as ProfileAdapter).setLiveData(it)
         })
     }
@@ -69,10 +81,10 @@ class HomeActivity : AppCompatActivity(),
     private fun changeLayoutManager() {
         viewBinding.apply {
             if (recyclerView.layoutManager is GridLayoutManager) {
-                recyclerView.layoutManager = LinearLayoutManager(this@HomeActivity)
+                recyclerView.layoutManager = LinearLayoutManager(context)
                 FABGridLayout.setImageResource(R.drawable.icon_grid)
             } else {
-                recyclerView.layoutManager = GridLayoutManager(this@HomeActivity, 2)
+                recyclerView.layoutManager = GridLayoutManager(context, 2)
                 FABGridLayout.setImageResource(R.drawable.icon_list)
             }
         }
@@ -80,7 +92,7 @@ class HomeActivity : AppCompatActivity(),
 
     override fun onClickProfileItem(view: View, profileData: ProfileData) {
 
-        val intent = Intent(this, ProfileDetailActivity::class.java)
+        val intent = Intent(context, ProfileDetailActivity::class.java)
         intent.putExtra("profileData", profileData)
         startActivity(intent)
     }
@@ -91,11 +103,11 @@ class HomeActivity : AppCompatActivity(),
 
     override fun onItemMoved(from: Int, to: Int) {
         profileListViewModel.moveProfileItem(from, to)
-        viewBinding.recyclerView.adapter?.notifyItemMoved(from, to)
+        //viewBinding.recyclerView.adapter?.notifyItemMoved(from, to)
     }
 
     override fun onItemSwiped(position: Int) {
         profileListViewModel.swipeProfileItem(position)
-        viewBinding.recyclerView.adapter?.notifyItemRemoved(position)
+        //viewBinding.recyclerView.adapter?.notifyItemRemoved(position)
     }
 }
