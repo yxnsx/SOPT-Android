c859f3c8001224c508257a944a2068b869a40c24
yxnsx <cw2cu96@gmail.com>
2020-11-10 15:10:44 +0900

    Create: ProfilViewPagerAdapter.kt 파일 생성 및 코드 작성
    # [타입] 제목
    # 제목의 길이는 최대 50글자 (영문 기준)
    # 제목 작성 후 한줄 공백 필수
    # 제목 타입 목록
    
    #	기능 추가	:	FEAT_ADD
    #	기능 수정	:	FEAT_FIX
    #	기능 보완	:	FEAT_UPDATE
    #	기능 삭제	:	FEAT_REMOVE
    #	문서 추가	:	DOCS_ADD
    #	문서 수정	:	DOCS_FIX
    #	문서 보완	:	DOCS_UPDATE
    #	문서 삭제	:	DOCS_REMOVE
    #	버그 수정	:	BUG_FIX
    #	파일 이동	:	FILE_MOVE
    #	파일명 변경	:	FILE_RENAME
    #	변수명 변경	:	VAR_RENAME
    #	테스트	:	TEST
    #	리팩토링	:	REFACTOR
    
    # 본문
    # 본문은 한 줄에 최대 70글자 (영문 기준)
    # 변경 사항, 변경 이유 포함

---
+++ app/src/main/java/com/yxnsx/sopt/week03/ProfileViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.yxnsx.sopt.week03
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+
+class ProfileViewPagerAdapter(fragmentManager: FragmentManager) :
+    FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+
+    override fun getItem(position: Int): Fragment = when(position){
+        0 -> ProfileInfoFragment()
+        1 -> ProfileOthersFragment()
+        else -> throw IllegalStateException("Unexpected position: $position")
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+}
