5ac8249ec0076cf8a1c0c626617b65cee7345c7a
yxnsx <cw2cu96@gmail.com>
2020-11-10 15:09:57 +0900

    Add: BottomNavProfileFragment.kt 파일에 viewBinding, viewPager 적용
    # [타입] 제목
    # 제목의 길이는 최대 50글자 (영문 기준)
    # 제목 작성 후 한줄 공백 필수
    # 제목 타입 목록
    
    #	기능 추가	:	FEAT_ADD
    #	기능 수정	:	FEAT_FIX
    #	기능 보완	:	FEAT_UPDATE
    #	기능 삭제	:	FEAT_REMOVE
    #	문서 추가	:	DOCS_ADD
    #	문서 수정	:	DOCS_FIX
    #	문서 보완	:	DOCS_UPDATE
    #	문서 삭제	:	DOCS_REMOVE
    #	버그 수정	:	BUG_FIX
    #	파일 이동	:	FILE_MOVE
    #	파일명 변경	:	FILE_RENAME
    #	변수명 변경	:	VAR_RENAME
    #	테스트	:	TEST
    #	리팩토링	:	REFACTOR
    
    # 본문
    # 본문은 한 줄에 최대 70글자 (영문 기준)
    # 변경 사항, 변경 이유 포함

---
+++ app/src/main/java/com/yxnsx/sopt/week03/BottomNavProfileFragment.kt
@@ -0,0 +1,41 @@
+package com.yxnsx.sopt.week03
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yxnsx.sopt.databinding.FragmentBottomNavProfileBinding
+
+
+class BottomNavProfileFragment : Fragment() {
+
+    private lateinit var viewBinding: FragmentBottomNavProfileBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = FragmentBottomNavProfileBinding.inflate(layoutInflater)
+        return viewBinding.root
+    }
+
+    // ui 작업 수행
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setTabLayout()
+    }
+
+    private fun setTabLayout() {
+        viewBinding.apply {
+            viewPager.adapter = ProfileViewPagerAdapter(childFragmentManager)
+            tabLayoutProfile.setupWithViewPager(viewBinding.viewPager)
+            tabLayoutProfile.apply {
+                getTabAt(0)?.text = "INFO"
+                getTabAt(1)?.text = "OTHERS"
+            }
+        }
+    }
+}
