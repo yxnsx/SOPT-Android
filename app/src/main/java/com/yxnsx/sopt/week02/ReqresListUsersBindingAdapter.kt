00dc8a4d36746ae8491f931f99fc2606c2cc7168
yxnsx <cw2cu96@gmail.com>
2020-12-10 13:29:37 +0900

    Modify: ReqresListUsersBindingAdapter.kt 파일의 bindDrawable 방식을 Glide로 변경

--- app/src/main/java/com/yxnsx/sopt/week02/ReqresListUsersBindingAdapter.kt
+++ app/src/main/java/com/yxnsx/sopt/week02/ReqresListUsersBindingAdapter.kt
@@ -3,8 +3,12 @@ package com.yxnsx.sopt.week02
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 @BindingAdapter("bindDrawable")
-fun bindDrawable(view: ImageView, @DrawableRes image: Int) {
-    view.setImageResource(image)
+fun bindDrawable(view: ImageView, image: String) {
+    Glide.with(view)
+        .load(image)
+        .circleCrop()
+        .into(view)
 }
